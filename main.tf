3318438714a21b6738c142f7e3477385bdc9b18d
borse-a2 <akshb14394@gmail.com>
2025-10-03 00:50:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -181,19 +181,22 @@ resource "azurerm_network_interface" "jumpbox" {
   tags = var.tags
 }
 
- resource "azurerm_monitor_autoscale_setting" "vmss_autoscale" {
+resource "azurerm_monitor_autoscale_setting" "vmss_autoscale" {
   name                = "example-autoscale"
   location            = azurerm_resource_group.vmss.location
   resource_group_name = azurerm_resource_group.vmss.name
+  target_resource_id  = azurerm_virtual_machine_scale_set.vmss.id
 
   profile {
     name = "defaultProfile"
+
     capacity {
       minimum = "1"
       maximum = "10"
       default = "2"
     }
 
+    # Scale-out rule: add 1 VM when CPU > 75%
     rule {
       metric_trigger {
         metric_name        = "Percentage CPU"
@@ -213,36 +216,30 @@ resource "azurerm_network_interface" "jumpbox" {
         cooldown  = "PT1M"
       }
     }
-  }
-rule {
-  metric_trigger {
-    metric_name        = "Percentage CPU"
-    metric_resource_id = azurerm_virtual_machine_scale_set.vmss.id
-    time_grain         = "PT1M"
-    statistic          = "Average"
-    time_window        = "PT5M"
-    time_aggregation   = "Average"
-    operator           = "LessThan"
-    threshold          = 25
-  }
 
-  scale_action {
-    direction = "Decrease"
-    type      = "ChangeCount"
-    value     = "1"
-    cooldown  = "PT5M"
-  }
-}
+    # Scale-in rule: remove 1 VM when CPU < 25%
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_virtual_machine_scale_set.vmss.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 25
+      }
 
-
-  notification {
-    email {
-      send_to_subscription_administrator = true
-      send_to_subscription_co_administrator = true
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
     }
   }
 
-  target_resource_id = azurerm_virtual_machine_scale_set.vmss.id
+  tags = var.tags
 }
 
 resource "azurerm_virtual_machine" "jumpbox" {
@@ -278,4 +275,3 @@ resource "azurerm_virtual_machine" "jumpbox" {
 
   tags = var.tags
 }
-
